b060e7b26a5c0fd8274576681fc43c5842580c76
akshaypratapsigmoid <123646244+akshaypratapsigmoid@users.noreply.github.com>
2023-04-18 19:52:36 +0530

    Update ITEM_PRICES.sql

--- dbt_assignment_snowflake/models/ITEM_PRICES.sql
+++ dbt_assignment_snowflake/models/ITEM_PRICES.sql
@@ -1,16 +1,35 @@
 {{
   config(
     materialized = 'incremental',
-    incremental_strategy = 'merge',
-    unique_key = ['version', 'country_code', 'item_key', 'year', 'month', 'price', 'user'],
     schema = 'CONS'
     )
-}}
+}}  
 
-with item_prices as (
-select 
-ips.version, cc.country_code, cc.country_name, ips.item_key, ips.item_name, ips.year, ips.month, ips.price, ips.user
-from country_codes as cc
-cross join item_prices_stg as ips
+with Country_code as (
+select * from DBT.STG.COUNTRY_CODES
+) ,
+Global_Item_Prices as (
+    select * from DBT.STG.GLOBAL_ITEM_PRICES
 )
-select * from item_prices
+
+
+select 
+version,
+country_key as country_code,
+country_name as country,
+item_key,
+item_name,
+year,
+month,
+price,
+user,
+case when cc.load_time > ip.load_time then
+cc.load_time
+else
+ip.load_time end
+as load_time
+from Country_code cc , Global_Item_Prices ip
+{% if is_incremental() %}
+where cc.load_time > (select max(load_time) from {{ this }})
+or ip.load_time > (select max(load_time) from {{ this }})
+{% endif %}
