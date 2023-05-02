113a1c31596cff95dc833d00f1c54481b12f7479
akshaypratapsigmoid <123646244+akshaypratapsigmoid@users.noreply.github.com>
2023-05-02 17:09:45 +0530

    Update ITEM_PRICES.sql

--- dbt_assignment_snowflake/models/ITEM_PRICES.sql
+++ dbt_assignment_snowflake/models/ITEM_PRICES.sql
@@ -1,35 +1,35 @@
 {{
-  config(
-    materialized = 'incremental',
-    schema = 'CONS'
+    config(
+        materialized = 'incremental',
+        schema = 'CONS',
+        incremental_strategy = 'delete+insert',
     )
-}}  
+}}
 
-with Country_code as (
-select * from DBT.STG.COUNTRY_CODES
-) ,
-Global_Item_Prices as (
-    select * from DBT.STG.GLOBAL_ITEM_PRICES
+with country_codes as (
+    select country_key, country_name, load_time from dbt_db.stg.country_codes
+),
+item_prices as (
+    select version, item_key, item_name, year, month, price, user, load_time from dbt_db.stg.item_prices_stg
 )
 
 
 select 
-version,
-country_key as country_code,
-country_name as country,
-item_key,
-item_name,
-year,
-month,
-price,
-user,
-case when cc.load_time > ip.load_time then
-cc.load_time
-else
-ip.load_time end
-as load_time
-from Country_code cc , Global_Item_Prices ip
+    version,
+    country_key as country_code,
+    country_name as country,
+    item_key,
+    item_name,
+    year,
+    month,
+    price,
+    user,
+    CASE WHEN cc.load_time > ip.load_time
+         THEN cc.load_time
+         ELSE ip.load_time
+    END as load_time
+ from country_codes cc, item_prices ip
 {% if is_incremental() %}
-where cc.load_time > (select max(load_time) from {{ this }})
-or ip.load_time > (select max(load_time) from {{ this }})
+      where cc.load_time > (select max(load_time) from {{ this }})
+    or ip.load_time > (select max(load_time) from {{this}})
 {% endif %}
